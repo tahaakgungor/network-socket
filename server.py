cbc8db8bce68870715afe0f23ac6085f368d9137
mehmetuner <mehmetuner15@gmail.com>
2023-04-25 13:23:57 +0300

    first commit

---
+++ server.py
@@ -0,0 +1,83 @@
+from aiohttp import web
+import socketio
+import requests
+from netmiko import ConnectHandler
+from getpass import getpass
+
+sio = socketio.AsyncServer(cors_allowed_origins='http://localhost:3000')
+app = web.Application()
+sio.attach(app)
+
+connections = {}
+
+@sio.event
+def connect(sid, environ):
+    print("connect ", sid)
+
+@sio.event
+async def command(sid, data):
+    print("command ", data)
+    print("message ", data['command'])
+    output = ""
+
+ 
+    output= connections[data['deviceId']].send_config_set(data['command'], exit_config_mode=False)
+
+    await sio.emit('output'+data['deviceId'],output, room=sid)
+
+    print("outtt:", output, "data: ", data['deviceId'])
+
+    
+    # response = connections[data['deviceId'][0][0]].send_command(data['command'])
+    # connections[data['deviceId'][0][0]].enable()
+    # print("response: ", response)
+    # output= connections[data['deviceId'][0][0]].send_config_set(data['command'])
+
+    # print("Connected to device successful"+"\n"+connections[data['deviceId'][0][0]].find_prompt())
+    # await sio.emit('output'+data['deviceId'][0][0],output, room=sid)
+
+    
+
+@sio.event
+async def createSSH(sid, data):
+    print("DATA: ", data[0])
+    devList = []
+    for i,device in enumerate(data):
+        print("DEV ", device, "i ", i)
+        req = requests.get('http://localhost:5000/devices/selected/'+device)
+        req = req.json()
+        req[0].pop('_id')
+        req[0].pop('name')
+        req[0].pop('__v')
+        devList.append(req)
+        print("DEVLIST: ", devList)
+        net_connect = ConnectHandler(**devList[i][0], timeout=10)
+        net_connect.enable()
+        connections[data[i]] = net_connect
+    print("DEVLIST: ", devList)
+    print("createSSH ", connections)
+
+
+
+    # net_connect = ConnectHandler(**device[0], timeout=10)
+    # net_connect.enable()
+   
+    # output= net_connect.send_config_set('do sh run')
+
+    # print("Connected to device successful"+"\n"+net_connect.find_prompt())
+    # print(output)
+
+  
+    # connections[data[0][0]] = net_connect
+    # print("createSSH ", connections[data[0][0]])
+    # out2=connections[data[0][0]].send_command('sh run')
+
+
+
+
+@sio.event
+def disconnect(sid):
+    print('disconnect ', sid)
+
+if __name__ == '__main__':
+    web.run_app(app, port=3002)
