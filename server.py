f5b5fd84fd4b5eb8000123b836cce0d26c6c1557
Taha Turan Akgüngör <tahaakgungor@gmail.com>
2023-05-17 16:43:28 +0300

    as

--- server.py
+++ server.py
@@ -24,22 +24,18 @@ async def privateCommand(sid,data):
     output = ""
     print("command ", data)
     print("conn",connections[data['devices']])
- 
     try:
         output = connections[data['devices']].send_config_set(data['command'], enter_config_mode=False, exit_config_mode=False, error_pattern=r'% Invalid input detected at')
-
         if output == "":
             output = 'Invalid command'
     except netmiko.exceptions.ConfigInvalidException as e:
-        output = f"\n{str(e)}\n" 
-   
-
+        output = f"\n{str(e)}\n"
 
     await sio.emit('output'+data['devices'],output, room=sid)
-    
 
     print("outtt:", output, "data: ", data['devices'])
     output = ""
+
 
 @sio.event
 async def command(sid, data):
@@ -48,35 +44,18 @@ async def command(sid, data):
     output = ""
     print("command ", data['deviceId'])
 
-
-
     try:
         output = connections[data['deviceId']].send_config_set(data['command'],cmd_verify=True, enter_config_mode=False, exit_config_mode=False, error_pattern=r'% Invalid input detected at')
 
-        
-       
     except netmiko.exceptions.ConfigInvalidException as e:
-        output = f"\n{str(e)}\n" 
-
-
+        output = f"\n{str(e)}\n"
 
     await sio.emit('output'+data['deviceId'],output, room=sid)
-    
 
     print("outtt:", output, "data: ", data['deviceId'])
     output = ""
 
 
-    
-    # response = connections[data['deviceId'][0][0]].send_command(data['command'])asd
-    # connections[data['deviceId'][0][0]].enable()
-    # print("response: ", response)
-    # output= connections[data['deviceId'][0][0]].send_config_set(data['command'])
-
-    # print("Connected to device successful"+"\n"+connections[data['deviceId'][0][0]].find_prompt())
-    # await sio.emit('output'+data['deviceId'][0][0],output, room=sid)
-
-    
 
 @sio.event
 async def createSSH(sid, data):
@@ -93,29 +72,10 @@ async def createSSH(sid, data):
         print("DEVLIST: ", devList)
         net_connect = ConnectHandler(**devList[i][0], timeout=10)
         net_connect.enable()
-
-    
-     
-     
         connections[data[i]] = net_connect
-      
+
     print("DEVLIST: ", devList)
     print("createSSH ", connections)
-
-
-
-    # net_connect = ConnectHandler(**device[0], timeout=10)
-    # net_connect.enable()
-   
-    # output= net_connect.send_config_set('do sh run')
-
-    # print("Connected to device successful"+"\n"+net_connect.find_prompt())
-    # print(output)
-
-  
-    # connections[data[0][0]] = net_connect
-    # print("createSSH ", connections[data[0][0]])
-    # out2=connections[data[0][0]].send_command('sh run')
 
 
 @sio.event
@@ -126,9 +86,32 @@ def disconnectSSH(sid, data):
     print("disconnectSSH ", connections)
 
 
+
 @sio.event
 def disconnect(sid):
-    print('disconnect ', sid)   
+    print('disconnect ', sid)
 
 if __name__ == '__main__':
     web.run_app(app, port=PORT)
+
+
+    # net_connect = ConnectHandler(**device[0], timeout=10)
+    # net_connect.enable()
+
+    # output= net_connect.send_config_set('do sh run')
+
+    # print("Connected to device successful"+"\n"+net_connect.find_prompt())
+    # print(output)
+
+
+    # connections[data[0][0]] = net_connect
+    # print("createSSH ", connections[data[0][0]])
+    # out2=connections[data[0][0]].send_command('sh run')
+
+  # response = connections[data['deviceId'][0][0]].send_command(data['command'])asd
+    # connections[data['deviceId'][0][0]].enable()
+    # print("response: ", response)
+    # output= connections[data['deviceId'][0][0]].send_config_set(data['command'])
+
+    # print("Connected to device successful"+"\n"+connections[data['deviceId'][0][0]].find_prompt())
+    # await sio.emit('output'+data['deviceId'][0][0],output, room=sid)
